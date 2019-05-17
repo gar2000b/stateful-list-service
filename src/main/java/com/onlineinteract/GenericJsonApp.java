7ab8d1efddbcc766d54066e0d6bae3e3d0eede73
Gary Black <gar2000b@yahoo.com>
2019-05-17 08:59:47 -0400

    More updates including pushing to flattenedPayloadValues

--- src/main/java/com/onlineinteract/GenericJsonApp.java
+++ src/main/java/com/onlineinteract/GenericJsonApp.java
@@ -1,6 +1,8 @@
 package com.onlineinteract;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,20 +10,37 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GenericJsonApp {
-	public static void main(String[] args) {
+	List<Map<String, Object>> flattenedPayloadValues;
+
+	public GenericJsonApp() {
+		flattenedPayloadValues = new ArrayList<>();
 		ObjectMapper om = new ObjectMapper();
 		String json = "{\"a\":\"a\",\"b\":\"b\",\"c\":[{\"d\":\"d\"},{\"e\":\"e\"}],\"f\":{\"g\":\"g\"},\"h\":[1,2,3],\"i\":4.3,\"j\":7,\"k\":true}";
 		try {
 			Map<String, Object> map = om.readValue(json, new TypeReference<Map<String, Object>>() {
 			});
 			processMap(map);
+			System.out.println("\nFlattened values:\n");
+			processFlattenedValues();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	private void processFlattenedValues() {
+		for (Map<String, Object> map : flattenedPayloadValues) {
+			for (Map.Entry<String, Object> entry : map.entrySet()) {
+				System.out.println(entry.getKey() + " - " + entry.getValue());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new GenericJsonApp();
+	}
+
 	@SuppressWarnings("unchecked")
-	private static void processMap(Map<String, Object> map) {
+	private void processMap(Map<String, Object> map) {
 		for (Map.Entry<String, Object> entry : map.entrySet()) {
 			System.out.println("\n" + entry.getKey() + ": " + entry.getValue());
 			if (entry.getValue() instanceof Map) {
@@ -37,21 +56,25 @@ public class GenericJsonApp {
 				System.out.println(entry.getKey() + " is not a List");
 			}
 			if (entry.getValue() instanceof String) {
+				injectMapEntryToFlattenedPayloadValues(entry);
 				System.out.println(entry.getKey() + " is a String");
 			} else {
 				System.out.println(entry.getKey() + " is not a String");
 			}
 			if (entry.getValue() instanceof Integer) {
+				injectMapEntryToFlattenedPayloadValues(entry);
 				System.out.println(entry.getKey() + " is an Integer");
 			} else {
 				System.out.println(entry.getKey() + " is not an Integer");
 			}
 			if (entry.getValue() instanceof Double) {
+				injectMapEntryToFlattenedPayloadValues(entry);
 				System.out.println(entry.getKey() + " is a Double");
 			} else {
 				System.out.println(entry.getKey() + " is not a Double");
 			}
 			if (entry.getValue() instanceof Boolean) {
+				injectMapEntryToFlattenedPayloadValues(entry);
 				System.out.println(entry.getKey() + " is a Boolean");
 			} else {
 				System.out.println(entry.getKey() + " is not a Boolean");
@@ -59,8 +82,14 @@ public class GenericJsonApp {
 		}
 	}
 
+	private void injectMapEntryToFlattenedPayloadValues(Map.Entry<String, Object> entry) {
+		Map<String, Object> hashMap = new HashMap<>();
+		hashMap.put(entry.getKey(), entry.getValue());
+		flattenedPayloadValues.add(hashMap);
+	}
+
 	@SuppressWarnings("unchecked")
-	private static void processList(List<Object> list) {
+	private void processList(List<Object> list) {
 		for (Object object : list) {
 			if (object instanceof Map) {
 				System.out.println("list entry is a Map");
@@ -75,25 +104,35 @@ public class GenericJsonApp {
 				System.out.println("list entry is not a List");
 			}
 			if (object instanceof String) {
+				injectListEntryToFlattenedPayloadValues(object);
 				System.out.println(object + " is a String");
 			} else {
 				System.out.println("list entry is not a String");
 			}
 			if (object instanceof Integer) {
+				injectListEntryToFlattenedPayloadValues(object);
 				System.out.println(object + " is an Integer");
 			} else {
 				System.out.println("list entry is not an Integer");
 			}
 			if (object instanceof Double) {
+				injectListEntryToFlattenedPayloadValues(object);
 				System.out.println(object + " is a Double");
 			} else {
 				System.out.println("list entry is not a Double");
 			}
 			if (object instanceof Boolean) {
+				injectListEntryToFlattenedPayloadValues(object);
 				System.out.println(object + " is a Boolean");
 			} else {
 				System.out.println("list entry is not a Boolean");
 			}
 		}
+	}
+
+	private void injectListEntryToFlattenedPayloadValues(Object object) {
+		Map<String, Object> hashMap = new HashMap<>();
+		hashMap.put("collection", object);
+		flattenedPayloadValues.add(hashMap);
 	}
 }
